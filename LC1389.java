096e97997a2f7b6e2ab8652dd18b9b2503ae81f7
Siddharth Samarth <73430719+sidsamgithub@users.noreply.github.com>
2021-05-30 17:02:11 +0100

    LC1389 ez Arrays

--- LC1389.java
+++ LC1389.java
@@ -30,6 +30,31 @@
  * 
  * 1 <= nums.length, index.length <= 100 nums.length == index.length 0 <=
  * nums[i] <= 100 0 <= index[i] <= i
+
+//======Looping Solution=========
+
+class Solution {
+  public int[] createTargetArray(int[] nums, int[] index) {
+
+    int target[] = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      if (index[i] == i) {
+        target[index[i]] = nums[i];
+      } else {
+        int temp;
+        for (int j = index[i]; j < i + 1; ++j) {
+          temp = target[j];
+          target[j] = nums[i];
+          nums[i] = temp;
+        }
+      }
+    }
+    return target;
+
+  }
+}
+
+
  */
 import java.util.ArrayList;
 import java.util.List;
