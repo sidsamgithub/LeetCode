d0bfa1a5c637b99db06eebf4534718f1c7f87bfb
Siddharth Samarth <73430719+sidsamgithub@users.noreply.github.com>
2021-06-28 21:18:02 +0100

    updated LC88 REVISIT Arrays in place ops

--- LC88.java
+++ LC88.java
@@ -40,24 +40,29 @@ nums2.length == n
 
 Follow up: Can you come up with an algorithm that runs in O(m + n) time?*/
 
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
         int p1=m-1;
-        int p2 = n-1;
-        int x = m+n-1;
-    
-        while(p2>=0)
+        int p2=n-1;
+        int i = m+n-1;
+        
+        while (p2>=0)
         {
-            if (p1>=0&&nums1[p1]>nums2[p2])
+            if (p1>=0 && nums1[p1]>nums2[p2])
             {
-                nums1[x--]=nums1[p1--];            
+                nums1[i]=nums1[p1];
+                p1--;
+                i--;
             }
             else
             {
-                nums1[x--]=nums2[p2--];
+                nums1[i]=nums2[p2];
+                p2--;
+                i--;
             }
-        
         }
+        
     }
 }
